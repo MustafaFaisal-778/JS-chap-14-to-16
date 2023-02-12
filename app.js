3ce30652cb3a3f4bd143a354d415d72bbbfec922
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-02-12 23:20:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+//Question#1
+// var student=[]
+
+//Question#2
+// var student =new Array();
+
+//Question#3
+// var student=["Ahmed","Ayaan","Ali"]
+
+//Question#4
+// var number=[1,2,3,4,5]
+
+//Question#5
+// var boolean=[true,false,null]
+
+//Question#6
+// var mixed=["Ali",2,false]
+
+//Question#7
+// var eduQualification=["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"]
+// console.log(eduQualification)
+// document.write("<h1>Qualification</h1>")
+// document.write("<p>1) "+eduQualification[0]+" </p>")
+// document.write("<p>2) "+eduQualification[1]+" </p>")
+// document.write("<p>3) "+eduQualification[2]+" </p>")
+// document.write("<p>4) "+eduQualification[3]+" </p>")
+// document.write("<p>5) "+eduQualification[4]+" </p>")
+// document.write("<p>6) "+eduQualification[5]+" </p>")
+// document.write("<p>7) "+eduQualification[6]+" </p>")
+// document.write("<p>8) "+eduQualification[7]+" </p>")
+ 
+///Question#8
+// var student=["Ahmed","Ali","Ayaan"]
+// var marks=[320,230,480]
+// var totalMarks=500
+// var per1=(marks[0]*100)/totalMarks
+// var per2=(marks[1]*100)/totalMarks
+// var per3=(marks[2]*100)/totalMarks
+// document.write("<p>Score of Ahmed is "+marks[0]+". Percentage "+per1+"%</p>")
+// document.write("<p>Score of Ali is "+marks[1]+". Percentage "+per2+"%</p>")
+// document.write("<p>Score of Ayaan is "+marks[2]+". Percentage "+per3+"%</p>")
+
+//Question#9
+// var color=["red","blue","green"]
+// document.write("<p>Array :"+color+"</p>")
+
+//A
+// var addColor=prompt("write color to add")
+// color.unshift(addColor)
+// document.write("<p>Updated Array :"+color+"</p>")
+//B
+// var addColor=prompt("write color to add")
+// color.push(addColor)
+// document.write("<p>Updated Array :"+color+"</p>")
+//C
+// var addColor=prompt("write color to add")
+// var addColor2=prompt("write another color to add")
+// color.splice(0,0,addColor,addColor2)
+// document.write("<p>Updated Array :"+color+"</p>")
+// //D
+// color.shift(addColor)
+// document.write("<p>Updated Array :"+color+"</p>")
+//E
+// color.pop(color)
+// document.write("<p>Updated Array :"+color+"</p>")
+//F
+// var addColor=+prompt("which index do you want to add yellow color")
+// color.splice(addColor,0,"yellow")
+// document.write("<p>Updated Array :"+color+"</p>")
+//G
+// var addColor=+prompt("which index do you want to delete array")
+// var addColor2=+prompt("how many number of colors do you want to delete")
+// color.splice(addColor,addColor2)
+// document.write("<p>Updated Array :"+color+"</p>")
+
+//Question#10
+// var scores=[320,230,480,120]
+// document.write("<p>Score of student: "+scores+"</p>")
+// scores.sort()
+// document.write("<p>Score of student: "+scores+"</p>")
+
+//Question#11
+// var cities=["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+// document.write("<p>Cities list: "+cities+"</p>")
+// var selected=[cities[2],cities[3]]
+// document.write("<p>Selected Cities: "+selected+"</p>")
+
+//Question#12
+// var arr = ["This", "is","my", "cat"];
+// a=arr.join(" ")
+// console.log(a)
+
